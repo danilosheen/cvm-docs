8bd726d5c61a7b4af5b099e86e18716565305c5e
danilosheen <c.danilo.f.silva@gmail.com>
2025-02-12 15:52:26 -0300

    inclusão de rotas e serviço que servirá como teste de dados injetados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FichaExcursaoComponent } from './pages/ficha-excursao/ficha-excursao.component';
+import { ContratoComponent } from './pages/contrato/contrato.component';
+import { ReciboComponent } from './pages/recibo/recibo.component';
+import { OrcamentoComponent } from './pages/orcamento/orcamento.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contrato', component: ContratoComponent },
+  { path: 'ficha-excursao', component: FichaExcursaoComponent },
+  { path: 'orcamento', component: OrcamentoComponent },
+  { path: 'recibo', component: ReciboComponent },
+];
