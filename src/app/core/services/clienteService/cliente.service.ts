363918d46acf0e73e067afb82347f6b4893c16f2
danilosheen <c.danilo.f.silva@gmail.com>
2025-04-13 20:52:16 -0300

    feat: implementação do serviço de autenticação e serviço de CRUD cliente

--- src/app/core/services/clienteService/cliente.service.ts
+++ src/app/core/services/clienteService/cliente.service.ts
@@ -1,13 +1,54 @@
 import { Injectable } from '@angular/core';
 import { ICliente } from '../../../interfaces/i-cliente';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from '../authService/auth-service.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() {}
+  private apiUrl = 'http://localhost:3000/api';
+  // private apiUrl = 'https://backend-cvm.vercel.app/api';
 
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  private getHeaders(): HttpHeaders {
+    const token = this.authService.getToken();
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
+  getAll(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/clientes`, { headers: this.getHeaders() });
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/cliente`, data, { headers: this.getHeaders() });
+  }
+
+  update(id: string, data: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/cliente/${id}`, data, { headers: this.getHeaders() });
+  }
+
+  delete(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/cliente/${id}`, { headers: this.getHeaders() });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+  // LocalStorage
   getAllClients(){
     let clientes: ICliente[] = JSON.parse(localStorage.getItem("clientes") || '[]');
     return clientes
--- src/app/core/services/clienteService/dist/cliente.service.js
+++ src/app/core/services/clienteService/dist/cliente.service.js
@@ -8,9 +8,33 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 exports.__esModule = true;
 exports.ClienteService = void 0;
 var core_1 = require("@angular/core");
+var http_1 = require("@angular/common/http");
 var ClienteService = /** @class */ (function () {
-    function ClienteService() {
+    // private apiUrl = 'https://backend-cvm.vercel.app/api';
+    function ClienteService(http, authService) {
+        this.http = http;
+        this.authService = authService;
+        this.apiUrl = 'http://localhost:3000/api';
     }
+    ClienteService.prototype.getHeaders = function () {
+        var token = this.authService.getToken();
+        return new http_1.HttpHeaders({
+            'Authorization': "Bearer " + token
+        });
+    };
+    ClienteService.prototype.getAll = function () {
+        return this.http.get(this.apiUrl + "/clientes", { headers: this.getHeaders() });
+    };
+    ClienteService.prototype.create = function (data) {
+        return this.http.post(this.apiUrl + "/cliente", data, { headers: this.getHeaders() });
+    };
+    ClienteService.prototype.update = function (id, data) {
+        return this.http.put(this.apiUrl + "/cliente/" + id, data, { headers: this.getHeaders() });
+    };
+    ClienteService.prototype["delete"] = function (id) {
+        return this.http["delete"](this.apiUrl + "/cliente/" + id, { headers: this.getHeaders() });
+    };
+    // LocalStorage
     ClienteService.prototype.getAllClients = function () {
         var clientes = JSON.parse(localStorage.getItem("clientes") || '[]');
         return clientes;
