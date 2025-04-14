b1b886e2e3ea36c0bd5d25f2ead20b663409755e
danilosheen <c.danilo.f.silva@gmail.com>
2025-04-14 17:35:55 -0300

    ajustes no navbar para botão de logout

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/authService/auth-service.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +10,10 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 })
 export class NavbarComponent {
 
+  constructor(private authService: AuthService, private router: Router){}
+
+  logout(){
+    this.authService.removeToken();
+    this.router.navigate(["/"]);
+  }
 }
